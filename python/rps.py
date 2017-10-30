3ef8efb0c18446ce10a694f91bb156b4215bba28
Susan Buck <susan@susanbuck.net>
2017-10-30 17:58:04 -0400

    Revert to previous version

--- python/rps.py
+++ python/rps.py
@@ -1,32 +1,27 @@
 import random
 
-# Parameters: a String `move`
-# Returns: Boolean for whether move is valid
-#
-# Valid moves include 'rock', 'paper', or 'scissors'`
-#
+
+
 def check_move(move):
-    if move == 'rock':
-        return True
-    if move == 'paper':
-        return True
-    if move == 'scissors':
+
+    if move == 'rock' or move == 'paper' or move == 'scissors':
         return True
     else:
         return False
 
-# Test the check_move function
-#print check_move('rock') # Expects: True
-#print check_move('paper') # Expects: True
-#print check_move('scissors') # Expects: True
-#print check_move('roc') # Expects: False
-#print check_move(1) # Expects: False
 
-# This function is provided; no edits are needed.
-# Parameters: None
-# Returns: String of 'rock', 'paper', or scissors'
-# Prompts the user for a move
-# Makes sure that move is valid; if it's not, continues to ask user for a move
+# Alternative version for check_move; just FYI:
+def check_move2(move):
+    return move == 'rock' or move == 'paper' or move == 'scissors'
+
+# print check_move('rock') # Expect: True
+# print check_move('paper') # Expect: True
+# print check_move('scissors') # Expect: True
+# print check_move('roc') # Expect False
+
+
+
+
 def get_player_move():
 
     # Prompt the user to enter their move
@@ -42,68 +37,67 @@ def get_player_move():
     # entered a valid move, so return it
     return move
 
-# Test this function
-#print 'You picked: ' + get_player_move()
+# print get_player_move()
 
-# This function is provided; no edits are needed
-# Parameters: None
-# Returns: String of 'rock', 'paper', or 'scissors'
+
+
 def get_computer_move():
     moves = ['rock', 'paper', 'scissors'];
     return random.choice(moves)
 
-#print get_computer_move() # Expected: 'rock', 'paper', or 'scissors'
-#print get_computer_move() # Expected: 'rock', 'paper', or 'scissors'
-#print get_computer_move() # Expected: 'rock', 'paper', or 'scissors'
+
 
 def judge(moveA, moveB):
-    if moveA == 'paper' and moveB == 'scissors':
-        return False
-    if moveA == 'scissors' and moveB == 'rock':
-        return False
+
+    # moveA is rock
     if moveA == 'rock' and moveB == 'paper':
         return False
-    else:
+
+    if moveA == 'rock' and moveB == 'scissors':
         return True
 
-#print judge('rock','paper') # Expected: False
-#print judge('rock','scissors') # Expected: True
-#print judge('paper','rock') # Expected: True
-#print judge('paper','scissors') # Expected: False
-#print judge('scissors','rock') # Expected: False
-#print judge('scissors','paper') # Expected: True
+    # moveA is paper
+    if moveA == 'paper' and moveB == 'rock':
+        return True
+
+    if moveA == 'paper' and moveB == 'scissors':
+        return False
+
+    # moveA is scissors
+    if moveA == 'scissors' and moveB == 'rock':
+        return False
+
+    if moveA == 'scissors' and moveB == 'paper':
+        return True
+
+
+# print judge('rock','paper') # Expected: False
+# print judge('rock','scissors') # Expected: True
+# print judge('paper','rock') # Expected: True
+# print judge('paper','scissors') # Expected: False
+# print judge('scissors','rock') # Expected: True
+# print judge('scissors','paper') # Expected: False
+
+
 
 def play():
 
-    print('Welcome to Welcome to Rock, Paper, Scissors!')
+    print('Welcome to Rock, Paper, Scissors!')
 
+    # Player goes
     player = get_player_move()
-    check_move(get_player_move)
 
+    # Computer goes
     computer = get_computer_move()
-
     print('The computer picked: ' + computer)
 
-
-#This was my sorry attempt at this portion of the game!
-# Finally had to give up and steal the solution for the part below. I was, as usual, overthinking
-# saved my try to remind me that judge is the function; the whole point is to use it as a tool, not have to re-instate it.
-#    judge (player, computer)
-#        if return == False:
-#            print('The computer won! ')
-#        elif return == True:
-#            print('You won!')
-#        elif player == computer:
-#            print('Tie! ')
-
-# Stolen from WCC
+    # Report results
     if player == computer:
         print('Tie!')
     elif(judge(player, computer) == True):
         print('You won!')
     else:
         print('The computer won!')
-
 
     # Prompt to play again
     play_again = raw_input('Play again? Type `y` or `n`: ')
@@ -113,5 +107,5 @@ def play():
     else:
         print('Thanks for playing!')
 
-# Run the game
+
 play()
